75a72b3a5ea9f229852873f43425eca683a9c4d5
calvinyhu <cahu@ucdavis.edu>
2017-05-25 19:46:13 -0700

    finished part c

--- Queries.pl
+++ Queries.pl
@@ -2,19 +2,19 @@
 % consult('Queries.pl').
 
 % a) Find all courses with 3 or 4 credits (fc_course).
-fc_course(L) :-
-  findall(Name, (course(Name, _, Units), (Units = 3; Units = 4)), L).
-%
-% find34(L3) :-
-%   findall(CourseName, (course(CourseName, _, Units), member(Units, [3,4])), L3).
+fc_course(Courses) :-
+  findall(CourseName, (course(CourseName, _, Units), member(Units, [3,4])), Courses).
+
+% fc_course(L) :-
+%   findall(Name, (course(Name, _, Units), (Units = 3; Units = 4)), L).
 %
 % find34(L3) :-
 %   findall(CourseName, course(CourseName, _, 3), L1),
 %   findall(CourseName, course(CourseName, _, 4), L2),
 %   append(L1, L2, L3).
 
-% b
+% b) Find all courses whose immediate pre-requisite is a course (imprereq).
 
-% c
-students(L) :-
-  bagof(Name, student(Name, (Current, member(ecs10, Current)), _), L).
+% c) Find names of all students in a course (students).
+students(NamesOfStudents, Course) :-
+  findall(Name, (student(Name, Classes, _), member(Course, Classes)), NamesOfStudents).
